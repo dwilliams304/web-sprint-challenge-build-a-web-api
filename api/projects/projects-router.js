8d106db2ce2244d9e1e4badc8d2f2b185afe4db3
dwilliams304 <d.williams0434@gmail.com>
2023-09-24 16:11:38 -0700

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,41 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Project = require('./projects-middleware');
+
+const router = express.Router();
+
+
+
+router.get('', (req, res) => {
+
+})
+
+
+router.get('/:id', (req, res) => {
+
+})
+
+
+router.post('', (req, res) => {
+
+})
+
+
+router.put('/:id', (req, res) => {
+
+})
+
+
+router.delete('/:id', (req, res) => {
+
+})
+
+
+router.get('/:id/actions', (req, res) => {
+
+})
+
+
+
+module.exports = router
