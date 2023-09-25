a2496b1d8806f9ea7d240acbfa7610e6b9d6ad0c
dwilliams304 <d.williams0434@gmail.com>
2023-09-24 18:04:57 -0700

    projects-router and projects-middleware

--- api/server.js
+++ api/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const server = express();
+const { logger } = require('./global-middleware/global-middleware');
+
+
 const actionsRouter = require('./actions/actions-router');
 const projectsRouter = require('./projects/projects-router');
 
 
+const server = express();
+
 // Configure your server here
 server.use(express.json());
 
-
+server.use(logger);
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter);
-
 
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
